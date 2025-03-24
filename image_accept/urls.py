35de7e29ec95303aff13f1f03ef5633d7852768d
Adeleye-Adeola <mightydeveloper500@gmail.com>
2025-03-24 09:33:59 +0100

    Used a different frontend

--- image_accept/urls.py
+++ image_accept/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.homepage, name = 'home')
+    path('', views.homepage, name = 'home'),
+    path('delete/<str:pk>/', views.delete_image, name = 'delete')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- image_accept/views.py
+++ image_accept/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import image_model
+from django.http import JsonResponse
 
 # Create your views here.
 def homepage(request):
@@ -9,10 +10,23 @@ def homepage(request):
         new_image = image_model(image = image_upload)
 
         new_image.save()
+        return JsonResponse({"message": "Image uploaded successfully"}, status=200)
 
     image_list = image_model.objects.all()
+
+    
 
     context = {
         'image_list': image_list
     }
     return render(request, 'base/index.html', context)
+
+
+def delete_image(request, pk):
+    if request.method == 'POST':
+        item = image_model.objects.get(id = pk)
+
+        item.delete()
+
+        return JsonResponse({"message": "Image deleted successfully"}, status=200)
+        return redirect('home')
